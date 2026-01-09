fe35a36382fcf1c4ff1f9c20855edb943421b21a
anurag <yourboyanurag777@gmail.com>
2026-01-09 11:07:24 +0530

    connected mongodb

---
+++ backend/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv ';
+
+export const Conncttodb = async ()=>{
+    try {
+        const connect  = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Connected to MongoDB ${connect.connection.host}`);
+        
+    } catch (error) {
+        console.log(`Error connecting to MongoDB ${error.message}`);
+        process.exit(1);
+    }
+}
