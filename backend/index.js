9ea422dc46d9b5c530f1b81612e4cc9718d77e92
anurag <yourboyanurag777@gmail.com>
2026-01-09 11:01:26 +0530

    Set-up the port

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,9 @@ app.use(express.json());
 app.get('/', (req,res)=>{
     res.send('started the project')
 })
+
+const PORT  = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`server is listening on port ${process.env.PORT}`);
+    
+})
