6df3f91590416db2791bb34cdcc19dc467b4a757
anurag <yourboyanurag777@gmail.com>
2026-01-10 09:39:46 +0530

    upcoming header

---
+++ clerk-react/src/Components/Header.tsx
@@ -0,0 +1,17 @@
+
+import { Badge } from "./ui/badge"
+
+
+
+export default function Header(){
+
+
+return(
+    <div className="flex items-centre justify-centre mx-xl  ">
+        <Badge/>
+
+         </div>
+)
+
+
+}
