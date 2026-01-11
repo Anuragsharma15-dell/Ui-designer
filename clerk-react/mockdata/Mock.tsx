89c91f8eb3cf5cd3dfe283d82f11b03d310e542e
anurag <yourboyanurag777@gmail.com>
2026-01-11 10:51:49 +0530

    frontend files

---
+++ clerk-react/mockdata/Mock.tsx
@@ -0,0 +1,49 @@
+ 
+ import { IceCream2Icon } from "lucide-react"
+ import { AxeIcon } from "lucide-react"
+ import { PiIcon } from "lucide-react"
+
+ 
+ 
+export default  function Mockdata(){
+    
+    type mocks = {
+         title: string;
+    icon:  SVGSVGElement;
+    size: number;
+    }
+        const mock :mocks= [{
+    title:"create a gaming app design  ",
+    icon:IceCream2Icon,
+    size:14
+ },
+      {
+         title:"give me mock design  ",
+    icon:AxeIcon,
+    size:14
+
+    },
+    {
+         title:"generate a card design ",
+    icon:PiIcon,
+    size:14
+
+    },
+    {
+         title:"give me a app design ",
+    icon:IceCream2Icon,
+    size:14
+
+    }
+
+]
+
+
+    
+
+    
+}
+
+ 
+
+ 
