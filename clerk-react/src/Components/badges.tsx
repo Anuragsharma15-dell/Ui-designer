56833e24a5d45fd1a21e138adfe53c5c1ca467f9
anurag <yourboyanurag777@gmail.com>
2026-01-11 10:40:37 +0530

    badges.tsx

---
+++ clerk-react/src/Components/badges.tsx
@@ -0,0 +1,13 @@
+import Mockdata from "mockdata/Mock";
+
+
+function Badges(){
+    return (
+        <div className="flex items-center my-6 px-4 py-2 max-h-10 w-40 ">
+            {
+                
+        
+            }
+        </div>
+    )
+}
